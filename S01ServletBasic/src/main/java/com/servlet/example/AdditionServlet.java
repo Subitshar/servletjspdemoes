d593fd10b8bcd468977c6939a88b9b097a7c98a7
subitsha <subitsha183001103@gmail.com>
2022-08-01 14:34:39 +0530

    changes

---
+++ S01ServletBasic/src/main/java/com/servlet/example/AdditionServlet.java
@@ -0,0 +1,29 @@
+package com.servlet.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class AdditionServlet extends GenericServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		System.out.println("Inside additionservlet service method");
+		int number1=Integer.parseInt(req.getParameter("number1"));
+		int number2=Integer.parseInt(req.getParameter("number2"));
+		int result=number1+number2;
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		out.println("<h3>Result = " + result + "</h3>");
+	}
+
+}
